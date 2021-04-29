f03cf2143fd201b08ff408cac5fc32eb8365e36a
Rita <hsen.yeh123@gmail.com>
2021-04-29 10:05:10 +0800

    feat: add Seeders

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
+const restaurantList = require('./restaurant.json')
 
 //connnect database
 mongoose.connect('mongodb://localhost/restaurant-list', {
@@ -13,4 +14,12 @@ mongoose.connect('mongodb://localhost/restaurant-list', {
 //取得資料庫連線狀態
 const db = mongoose.connection
 db.on('error', () => { console.log('Error!') })
-db.once('open', () => { console.log('Mongodb connected!') })
+db.once('open', () => {
+  console.log('Mongodb connected!')
+  //取得餐廳JSON檔
+  for (let i = 0; i < restaurantList.results.length; i++) {
+    Restaurant.create(restaurantList.results[i])
+  }
+  console.log('Done!')
+})
+
