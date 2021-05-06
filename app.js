f00b7d23cac45ce5e4b823f92dc4dc259dc4b883
Rita <hsen.yeh123@gmail.com>
2021-05-06 13:37:33 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const restaurant = require('./models/restaurant')
+const methodOverride = require('method-override')
 
 //1-1 直接取用JSON檔的方法
 //const restaurantList = require('./restaurant.json')
@@ -12,7 +14,7 @@ const port = 3000
 
 // 引用 body-parser
 const bodyParser = require('body-parser')
-const restaurant = require('./models/restaurant')
+
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -37,6 +39,9 @@ app.use(express.static('public'))
 //layout setup
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
@@ -68,7 +73,7 @@ app.get('/restaurants/:id', (req, res) => {
 })
 
 //刪除餐廳 
-app.post('/restaurants/:id/delete', (req, res) => {
+app.delete('/restaurants/:id', (req, res) => {
 
   const id = req.params.id
   return Restaurant.findById(id)
@@ -100,7 +105,7 @@ app.get('/restaurants/:id/edit', (req, res) => {
 })
 
 
-app.post('/restaurants/:id/edit/update', (req, res) => {
+app.put('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
     .then(restaurant => {
