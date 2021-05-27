0590bda0e311a0efe6163ff45b1874b93d12823d
Rita <hsen.yeh123@gmail.com>
2021-05-27 21:37:04 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+// 引用 body-parser
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 require('./config/mongoose')
-
 
 
 const app = express()
 const port = 3000
 
-// 引用 body-parser
-const bodyParser = require('body-parser')
+//layout setup
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,12 +30,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //use public file
 app.use(express.static('public'))
 
-//layout setup
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+
+
+
 
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
+
+
 
 app.use(routes)
 
